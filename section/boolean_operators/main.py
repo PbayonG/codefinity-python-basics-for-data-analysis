ce6ecf5b3c6dd105c75766244647c09f7b33233d
PbayonG <>
2026-04-25 16:21:43 +0000

    Boolean Operators

---
+++ section/boolean_operators/main.py
@@ -0,0 +1,8 @@
+# Step 1: Define una variable para el costo total de la compra
+total_cost = 25.00
+
+# Step 2: Comprueba si el costo total alcanza el umbral de descuento (>= 20.00)
+discountEligible = total_cost >= 20.00
+
+# Step 3: Imprime el resultado
+print("Is the purchase eligible for a discount?", discountEligible)
