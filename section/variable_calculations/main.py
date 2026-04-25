7aa29b2dfabc85b064f5ed313b1023923ab769f9
PbayonG <>
2026-04-25 16:21:03 +0000

    Variable calculations

---
+++ section/variable_calculations/main.py
@@ -0,0 +1,10 @@
+# Crear variables según instrucciones
+item_name = "Soda"           # Asignar el nombre del artículo
+item_price = 6.99            # Asignar el precio por unidad (float)
+purchase_quantity = 5        # Asignar la cantidad a comprar (int)
+
+# Calcular el costo total
+total_cost = item_price * purchase_quantity
+
+# Prueba de salida
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
