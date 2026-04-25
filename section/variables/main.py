04ac3df6ffcea34fdf13a5afc579a7ad045db233
PbayonG <>
2026-04-25 16:20:56 +0000

    Variables

---
+++ section/variables/main.py
@@ -0,0 +1,11 @@
+# Definir variables según la tarea
+item_name = "Eggs"               # Asignar un string para el nombre del artículo
+item_price = 3.99                # Asignar un float para el precio por unidad
+item_stock_quantity = 120        # Asignar un integer para la cantidad en stock
+in_stock = True                  # Asignar un booleano indicando si está en stock
+
+# Testing
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
